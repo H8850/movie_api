d91ae8e07859299a6d2cfae7cb40928d8b3dcbae
H8850 <70569109+H8850@users.noreply.github.com>
2020-11-13 16:20:23 +0100

    added movies instead of books

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const app = express();
 //creates an array of objects
 let topMovies = [
     {
-        title: 'Harry Potter and the Sorcer\'s Stone',
-        author: 'J.K Rowling'
+        title: 'Die Hard',
+        director: 'John Mctiernan'
     },
     {
-        title: 'Lord of the Rings',
-        author: 'J.R.R. Tolkein'
+        title: 'Star Wars',
+        director: 'George Lucas'
     },
     {
-        title: 'Twilight',
-        autho: 'Stephanie Meyer'
+        title: 'Spirited Away',
+        director: 'Hayao Miyazaki'
     }
 ];
 
 //GET requests
 app.get('/', (req, res) => {
-    res.send('Welcome to my book club');
+    res.send('Welcome to my movie club');
 });
 
 app.get('/documentation', (req, res) => {
     res.sendFile('public/documentation.html', { root: __dirname});
 });
 
-app.get('/books', (req, res) => {
+app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
